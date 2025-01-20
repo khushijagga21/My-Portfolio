2228da6c1663099998fcbd20d8b72ef28120f2d8
khushijagga21 <khushijagga14@gmail.com>
2025-01-20 11:24:29 -0700

    adding files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
     
 <nav className="bg-gray-600 text-white p-6  fixed top-0 left-0 right-0 z-50 bg-black text-white p-6" > 
   <div className="container mx-auto flex justify-between items-center">
-    <h1 classNameName="text-3xl font-bold">KJ</h1> 
+    <h1 className="text-3xl font-bold">KJ</h1> 
     <div className="flex space-x-4"> 
       <Link to="/" className="hover:text-gray-200 text-xl">Home</Link>
       <Link to="/projects" className="hover:text-gray-200 text-xl">Work</Link>
